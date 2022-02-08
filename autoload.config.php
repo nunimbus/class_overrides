6270301ba33f08e69df66c6c5fffc829a71bcbcc
summersab <andrew+github@platypusinnovations.com>
2022-02-08 17:10:05 -0600

    Edit paths for apps to custom_apps

--- autoload.config.php
+++ autoload.config.php
@@ -3,7 +3,7 @@
 foreach (\OC::$composerAutoloader->getRegisteredLoaders() as $path=>$loader) {
 	if (isset ($loader->getPrefixesPsr4()['OC\\']) || array_key_exists('OC\\', $loader->getPrefixesPsr4())) {
 		$loader->unregister();
-		require_once __DIR__ . '/../apps/class_overrides/composer/composer/autoload_real.php';
+		require_once __DIR__ . '/../custom_apps/class_overrides/composer/composer/autoload_real.php';
 		\OC::$composerAutoloader = ComposerAutoloaderInitClassOverrides::getLoader();
 	}
 }
